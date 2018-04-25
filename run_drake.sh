a6b03fbb63a04ebf90fbb510ba2a7a6fd343e805
Winter Guerra <winterg@mit.edu>
2018-04-25 11:51:06 -0400

    Added a script to run the opensource version of drake from. Mounts the directory as /notebooks.

---
+++ run_drake.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+xhost +local:root; docker run -i --rm -e DISPLAY -e QT_X11_NO_MITSHM=1 -v /tmp/.X11-unix:/tmp/.X11-unix --privileged -v "$(pwd)"":"/notebooks -t drake bash; xhost -local:root
