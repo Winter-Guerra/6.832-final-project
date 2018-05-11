a78859d2460caf91ffca895e5b4b1ec28a56ed85
Winter Guerra <winterg@mit.edu>
2018-05-11 17:32:17 -0400

    Fixed a compile error in the state helper function

--- src/getXVectorFromStateStruct.m
+++ src/getXVectorFromStateStruct.m
@@ -7,7 +7,8 @@ n_x = 6;
 % Note: in the 2D case for the x_vector, we are defining theta as counter clockwise 
 % (e.g. along an axis out of the page). This corresponds to theta_2d = -theta_y for the 3d case. 
 
-theta = -rotm2eul(s.rotation)(2);
+euler = rotm2eul(s.rotation);
+theta = -euler(2);
 theta_dot = -angularVelocity(2);
 
 x = [ s.position(1); s.position(3); theta; s.velocity(1); s.velocity(3); theta_dot ];
