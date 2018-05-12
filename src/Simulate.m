947e707e16503ab8803310187ac59d3625150199
Winter Guerra <winterg@mit.edu>
2018-05-12 17:32:12 -0400

    Simplified visualization code and forward simulation code to only use X vectors and not do euler conversions.

--- src/Simulate.m
+++ src/Simulate.m
@@ -1,22 +1,17 @@
-function Simulate(traj,state, fig)
+function Simulate(x_vec, fig)
+
+% X_vec is size (t, len(x)). E.g. one row for each t_i.
+[num_t, len_x] = size(x_vec); 
 
 figure(fig);
 holdFig = ishandle(fig);
 xlabel('x')
 ylabel('y')
 zlabel('z')
-%if ~holdFig
-%     axis equal
-%     grid on
-%     view([-111,-31])
-%     axis([-5 5 -5 5 -4 -1]);
-%     camroll(180)
-% end
 
 axis equal;
 grid on;
-%axis([-10 10 -10 10 -10 10]);
-plot3 (traj(:,1), traj(:,2), traj(:,3));
+plot3 (x_vec(:,1), zeros(num_t, 1), x_vec(:,2));
 hold on;
 view(0,0);
 
@@ -24,12 +19,11 @@ h  = draw_drone([], 'b',1,0);
 ht = hgtransform('Parent', gca); 
 set(h, 'Parent', ht);
 
-for i = 1:size(state,1)
+for i = 1:num_t
     hold off;
-    %txy = makehgtform('translate', state(i,1:3),'zrotate', state(i,4), 'yrotate', state(i,5), 'xrotate', state(i,6));
     transformMatrix = eye(4);
-    transformMatrix(1:3,1:3) = state(i).rotation;
-    transformMatrix(1:3,4) = state(i).position'; 
+    transformMatrix(1:3,1:3) = eul2rotm([0 x_vec(i,3) 0]);
+    transformMatrix(1:3,4) = [x_vec(i,1) 0 x_vec(i,2)]'; 
     set (ht, 'Matrix', transformMatrix);
     pause(.1);
 end
--- src/simulate2Dprojectile.m
+++ src/simulate2Dprojectile.m
@@ -19,9 +19,10 @@ constants.g  = -g;
 constants.J  = J;
 simulationTime = 200 / sim_dt;
 constants.tmax = 3*g;
+constants.dt = 0.01;
+% Define the width of the quadrotor (distance between motors)
+constants.baseline = 0.1;
 
-% Initialize the state
-state.position = [0; 0; 0];
 T = [0];
 
 %% Compute the projectile equations
@@ -32,21 +33,14 @@ vx    = v0 * cos(theta);
 vz    = v0 * sin(theta);
 
 %% Simulate the loop
-thetas = [];
-state.rotation = eul2rotm([0 0 0]);
+x_vec = [];
 x = [1 0 theta vx vz 0];
 t = 0:0.01:0.5;
 traj   = zeros(length(t), 6);
 pitch  = zeros(length(t), 1);
-states = [];
-pitch_vec = [];
 
 
-for i = 2:length(t)
-    constants.dt = 0.01;
-    % Define the width of the quadrotor (distance between motors)
-    constants.baseline = 0.1;
-    
+for i = 2:length(t)    
     % Calculate input from controller
     u = [50.0 0]; % F_1, F_2
     
@@ -54,16 +48,10 @@ for i = 2:length(t)
     x = quadrotorDynamics2d(x, u, constants);
     
     % Populate vector of states for later visualization
-    state.position = [x(1) 0 x(2)];
-    state.velocity = [x(4) 0 x(5)];
-    state.angularVelocity = [0 x(6) 0];
-    state.rotation = eul2rotm([0 x(3) 0]);
+    x_vec = [x_vec; x];    
+    %traj(i,1:3) = [x(1) 0 x(2)]';
+    %traj(i,4:6) = rotm2eul(x(3), 'zyx');
     
-    traj(i,1:3) = state.position';
-    traj(i,4:6) = rotm2eul(state.rotation, 'zyx');
-%     disp(state.velocity');
-    pitch(i) = traj(i,5);
-    states = [states; state];
     T = [T, T(end) + constants.dt];
 end
 
@@ -75,4 +63,4 @@ axis equal;
 
 figure(2);
 
-Simulate(traj,states, 2);
+Simulate(x_vec, 2);
