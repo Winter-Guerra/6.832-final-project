173e356ed435a3d8546af85c673bf80ad6ac5507
Winter Guerra <winterg@mit.edu>
2018-05-12 20:51:57 -0400

    Made visualization loop.

--- src/Visualize.m
+++ src/Visualize.m
@@ -19,12 +19,14 @@ h  = draw_drone([], 'b',1,0);
 ht = hgtransform('Parent', gca); 
 set(h, 'Parent', ht);
 
-for i = 1:num_t
-    hold off;
-    transformMatrix = eye(4);
-    transformMatrix(1:3,1:3) = eul2rotm([0 x_vec(i,3) 0]);
-    transformMatrix(1:3,4) = [x_vec(i,1) 0 x_vec(i,2)]'; 
-    set (ht, 'Matrix', transformMatrix);
-    pause(.1);
-end
+while(true)
+    for i = 1:num_t
+        hold off;
+        transformMatrix = eye(4);
+        transformMatrix(1:3,1:3) = eul2rotm([0 x_vec(i,3) 0]);
+        transformMatrix(1:3,4) = [x_vec(i,1) 0 x_vec(i,2)]'; 
+        set (ht, 'Matrix', transformMatrix);
+        pause(.1);
+    end
+end    
 end
