295e8cb5b8d938d8b1f1acbc0a8f047a781c13a8
Winter Guerra <winterg@mit.edu>
2018-05-13 15:01:39 -0400

    Fixed 2D trajectory generator. Working on feeding the trajectory into TVLQR.

--- src/getConstants.m
+++ src/getConstants.m
@@ -5,6 +5,7 @@ constants.g  = -9.8;
 constants.J  = 5e-2;
 constants.tmax = 3*constants.g;
 constants.dt = 0.01;
+constants.radius = 1;
 % Define the width of the quadrotor (distance between motors)
 constants.baseline = 0.1;
 end
